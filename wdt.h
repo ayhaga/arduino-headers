6f0bab3fb511dcedf40530a4b7aed5b32db4c9c9
Simon Zimmermann <simonz05@gmail.com>
2010-10-24 20:20:40 +0200

    watchdog headers for ATtinyX5 with interrupt support

---
+++ wdt.h
@@ -0,0 +1,31 @@
+/*
+ * This file is part of arduino-headers.
+ *
+ * arduino-headers is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or(at your option) any later version.
+ *
+ * arduino-headers is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with arduino-headers.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if defined(__AVR_ATmega8__)
+#  error "arduino/wdt.h: Not implemented for ATmega8 chips yet"
+#elif defined(__AVR_ATmega168__) \
+   || defined(__AVR_ATmega328P__)
+#  error "arduino/wdt.h: Not implemented for ATmega328P chips yet"
+#elif defined(__AVR_ATmega1280__)
+#  error "arduino/wdt.h: Not implemented for ATmega1280 chips yet"
+#elif defined(__AVR_ATtiny25__) \
+   || defined(__AVR_ATtiny45__) \
+   || defined(__AVR_ATtiny85__)
+#  include <arduino/ATtinyX5/wdt.h>
+#else
+#  error "arduino/wdt.h: Unknown chip type"
+#endif
